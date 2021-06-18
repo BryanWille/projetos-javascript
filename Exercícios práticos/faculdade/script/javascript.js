bcb0536ac34e0b72b1a79f78ab9eb3a08100a952
bryanwille <bryanelliw@gmail.com>
2021-06-18 12:51:50 -0300

    Site do exercício

--- Exercícios práticos/faculdade/script/javascript.js
+++ Exercícios práticos/faculdade/script/javascript.js
@@ -1,5 +1,26 @@
-elNome = document.getElementById("nome");
-nomeP = document.getElementById("nomeP");
 function nome(){
-    nomeP.innerText = `Digite o sálario de ${String(elNome.value)}`;
+    elNome = document.getElementById("nome");
+    nomeP = document.getElementById("nomeP");
+    nomeP.innerText = `Digite o salário de ${String(elNome.value)}:`;
+}
+
+function imposto(){
+    var taxa = valorFixo = plBruto = plLiquido = impRenda = 0;
+    elSalario = document.getElementById("salario");
+    salario = Number(elSalario.value);
+    if (salario < 300){
+        taxa =  0.70;
+        valorFixo = 500;
+    } else if (salario < 1000){
+        taxa = 0.50;
+        valorFixo = 200;
+    } else {
+        taxa = 0.30;
+        valorFixo = 0;
+    }
+    plBruto = valorFixo + (salario * taxa);
+    impRenda = 0.25 * plBruto;
+    plLiquido = plBruto - impRenda;
+    elResult = document.getElementById("resultado");
+    elResult.innerText = `A participação nos lucros de ${elNome.value} foi de R$${plLiquido}!`;
 }
