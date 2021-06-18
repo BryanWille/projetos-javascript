017359d6bac7266c394e74cb43e845b32f97f195
Bryan <bryanelliw@gmail.com>
2021-06-17 22:37:26 -0300

    dasd

--- Exercícios práticos/Euler/script/fatorPrimo.js
+++ Exercícios práticos/Euler/script/fatorPrimo.js
@@ -1,19 +1,5 @@
-const primo = 600851475143;
-let conta = 0; 
+const primo = 643;
 var array = new Array(primo) ;
-for (var cont = 1; cont < primo; cont+=2){
-    if(primo % cont == 0){
-        var numero = cont;
-        for (var aux = 2; aux < numero; aux++ ){
-            if(numero % aux == 0){
-                array[conta] = cont;
-                conta ++;
-            }
-        }
-        
-    }
-}
-var arrayLength = array.length;
-for (var cont = 0; cont < arrayLength; cont++){
-    document.write("<p>" +array[cont] + "</p>");
+for (var cont = 1; cont < primo; cont++){
+        document.write("<p>" +cont +"</p>")
 }
